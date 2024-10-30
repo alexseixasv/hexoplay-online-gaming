40dded5b148665428538bfe662d0ee5e20eba984
Alex Seixas Venancio <108166849+alexseixasv@users.noreply.github.com>
2024-10-30 11:06:00 -0300

    [UPD] Login.vue
    Adjusted Dark Mode

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -13,33 +13,33 @@
                 </h1>
                 <p class="text-gray-300 text-lg" v-html="$t('homepage.description')"></p>
             </div>
-            <div class="w-full mx-auto rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
+            <div class="w-full mx-auto rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                <h1 class="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
                     {{ $t('login.subtitle') }}
                 </h1>
                 <form @submit.prevent="handleLogin" class="space-y-4 md:space-y-6">
                     <div>
-                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ $t('login.email') }}</label>
+                    <label for="email" class="block mb-2 text-sm font-medium text-white">{{ $t('login.email') }}</label>
                     <input
                         v-model="email"
                         type="email"
                         name="email"
                         id="email"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        class="border rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
                         placeholder="email@provider.com"
                         required
                     />
                     </div>
                     <div>
-                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ $t('login.password') }}</label>
+                    <label for="password" class="block mb-2 text-sm font-medium text-white">{{ $t('login.password') }}</label>
                     <input
                         v-model="password"
                         type="password"
                         name="password"
                         id="password"
                         placeholder="••••••••"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        class="border border-gray-300 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         required
                     />
                     </div>
@@ -50,8 +50,8 @@
                     >
                     {{ $t('login.button') }}
                     </button>
-                    <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                    {{ $t('login.noAccount') }} <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">{{ $t('login.clickHere') }}</a>
+                    <p class="text-sm font-light text-gray-400">
+                    {{ $t('login.noAccount') }} <a href="#" class="font-medium text-white hover:underline">{{ $t('login.clickHere') }}</a>
                     </p>
                 </form>
                 </div>
